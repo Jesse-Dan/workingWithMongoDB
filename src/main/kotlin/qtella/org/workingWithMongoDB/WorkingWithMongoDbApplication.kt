e1f17a45e682061dfa5b99fda232006ff0255a87
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2023-10-22 20:42:03 +0100

    working with mongo db

---
+++ src/main/kotlin/qtella/org/workingWithMongoDB/WorkingWithMongoDbApplication.kt
@@ -0,0 +1,11 @@
+package qtella.org.workingWithMongoDB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkingWithMongoDbApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkingWithMongoDbApplication>(*args)
+}
---
+++ src/test/kotlin/qtella/org/workingWithMongoDB/WorkingWithMongoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package qtella.org.workingWithMongoDB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkingWithMongoDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
